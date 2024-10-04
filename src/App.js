77cfcd8e366741deeaba659c4862cb3bec903703
RaghavRs2369 <raghavendran1999@gmail.com>
2024-10-05 00:40:06 +0530

    Creating Portals with ReactDOM

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Sidebar from "./components/Sidebar";
 import AccordionPage from "./pages/AccordionPage";
 import ButtonPage from "./pages/ButtonPage";
 import DropdownPage from "./pages/DropdownPage";
+import ModalPage from "./pages/ModalPage";
 
 ///////////////////////////////////////////
 
@@ -11,14 +12,17 @@ function App() {
     <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
       <Sidebar />
       <div className="col-span-5">
-        <Route path="/accordion">
-          <AccordionPage />
-        </Route>
         <Route path="/">
           <DropdownPage />
         </Route>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
         <Route path="/buttons">
           <ButtonPage />
+        </Route>
+        <Route path="/modal">
+          <ModalPage />
         </Route>
       </div>
     </div>
